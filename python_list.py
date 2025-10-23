26807192e82370e12fe0c2dccbf684f187ba1636
Zuhriddin2010 <zuhriddin2010@gmail.com>
2025-10-23 16:25:43 +0500

    List

---
+++ python_list.py
@@ -0,0 +1,51 @@
+# list = ["apple", "banana", "cherry"]
+# print(list[-3].upper())
+# print(list[-1].replace("cherry", "orange"))
+# print(list[0].title())
+# print(list[0].upper())
+
+
+# ismlar = ['bobur', 24, 'anvar', 34]
+# #           0       1      2     3
+# #         -4      -3     -2    -1
+# print(ismlar)
+
+# ism1 = ismlar[2]
+# # print(ism1.title())
+
+# moshinalar = []
+# print(moshinalar)
+
+# moshinalar.append('nexia')
+# print(moshinalar)
+
+# moshinalar.append('gentra')
+# print(moshinalar)
+
+# moshinalar.append('malibu')
+# print(moshinalar)
+
+# moshinalar.append('bmw')
+# print(moshinalar)
+
+# moshinalar.insert(2, 'lacetti')
+# print(moshinalar)
+
+# moshinalar.insert(-1, 'toyota')
+
+# print(moshinalar)
+
+# moshinalar.remove('nexia')
+# print(moshinalar)
+
+# j = moshinalar.pop(2) # masihnalar[2]
+# print(j)
+
+
+ism = ["Ixtiyor", "Kamron", "Bexruz"]
+print(ism)
+
+sonlar = [-30, 50, 1111111111, 99,99]
+print(sonlar)
+print(sonlar[2] + sonlar[3])
+print(sonlar[-1] - sonlar[0])
