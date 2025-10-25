c241f04f8ae15082f423a738a1156ed5e4f80701
Zuhriddin2010 <zuhriddin2010@gmail.com>
2025-10-25 16:40:17 +0500

    list

---
+++ list_funksiyalar.py
@@ -0,0 +1,47 @@
+"""
+25.10.2025
+ Ro`yhatlar bilan ishlash.Metodlar va functionlar.
+  metods(.sart())
+    function(sum(), list(), max(), min())
+    ro`yhatlardan nusha olish
+    Type() o`zgarmas royhat
+ """
+
+# davlatlar = ['ozbekiston', 'aqsh', 'rossiya', 'xitoy', 'malayziya', 'singapur', 'korea', 'yaponiya']
+# mashinalar = ["bmw","damas","cobalt","nexia","audi","gentra","lacetti","toyota"]
+# sonlar = list((12,45,78,23,56,89,-34,67,90,-11,43))
+
+
+# print(davlatlar)
+# print(mashinalar)
+# print(sonlar)
+
+# mashinalar.sort() # mashinalar ro`yhatini alifbo bo`yicha saralash
+# print(mashinalar)
+
+# sonlar.sort(reverse=True) # sonlar ro`yhatini teskari tartibda saralash
+# print(sonlar)
+
+# sonlar1 = list(range(0,101)) # 0 dan 100 gacha bo`lgan sonlar ro`yhati
+# print(sonlar1)
+
+# sonlar2 = list(range(0,30,2)) # 0 dan 30 gacha bo`lgan juft sonlar ro`yhati
+# print(sonlar2)
+# print(len(sonlar2)) # sonlar2 ro`yhatidagi elementlar soni
+
+# sonlar3 = list((10,21,32,43,54,65,76,87,98,109,-120,-11,-22,-33)) 
+# print(max(sonlar3)) # sonlar3 ro`yhatidagi eng katta sonni chiqarish
+# print(min(sonlar3)) # sonlar3 ro`yhatidagi eng kichik sonni chiqarish
+# print(sum(sonlar3)) # sonlar3 ro`yhatidagi sonlar yig`indisini chiqarish
+
+# davlatlar = ['ozbekiston', 'aqsh', 'rossiya', 'xitoy', 'malayziya', 'singapur', 'korea', 'yaponiya']
+
+# country = davlatlar 
+
+# print(davlatlar)
+# print(country)
+
+# country.insert(0, 'kanada') # country ro`yhatiga boshiga 'kanada' davlatini qo`shish
+# print(country)
+# print(davlatlar) # davlatlar ro`yhatiga hech qanday o`zgartirish kiritilmaganligini ko`rish
+
