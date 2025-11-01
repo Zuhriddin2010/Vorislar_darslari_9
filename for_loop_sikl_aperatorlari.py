515506854a0f2e8d6de9195ff35c008d2a2ac0e0
Zuhriddin2010 <zuhriddin2010@gmail.com>
2025-11-01 16:19:00 +0500

    ..

---
+++ for_loop_sikl_aperatorlari.py
@@ -0,0 +1,26 @@
+oquvchilar = ["kamolbek","boburbek","athambek","anvarbek","lazizbek","umit"]
+
+# print(f"assalo alaykum hurmatli,{oquvchilar[0]},ishlar qanday")
+# print(f"assalo alaykum hurmatli,{oquvchilar[1]},ishlar qanday")
+# print(f"assalo alaykum hurmatli,{oquvchilar[2]},ishlar qanday")
+# print(f"assalo alaykum hurmatli,{oquvchilar[3]},ishlar qanday")
+# print(f"assalo alaykum hurmatli,{oquvchilar[4]},ishlar qanday")
+# print(f"assalo alaykum hurmatli,{oquvchilar[5]},ishlar qanday")
+
+# for ism in oquvchilar:
+#     print(f"assalom alaykum {ism.title()}")
+#     print(f"xayr hurmatli {ism.upper()}")
+
+raqmlar = list((1,5,9,-8,63))
+
+# for son in raqmlar:
+#     print(f"{son} ninig qiymati: {son**2} ga teng")
+
+# sonlar = list(range(1,91))
+# for so in sonlar:
+#     print(f"{so}/9 ga javobi {so/9}")
+
+sonlar1 = list(range(27,53,7))
+
+for sonlar in sonlar1:
+    print(f"{sonlar} 4 darjasini topish {sonlar**4}")
