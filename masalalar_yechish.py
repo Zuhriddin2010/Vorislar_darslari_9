6cbb77b46ad304f1f078ccddc71032b2f841118f
Zuhriddin2010 <zuhriddin2010@gmail.com>
2025-11-04 16:32:02 +0500

    .

---
+++ masalalar_yechish.py
@@ -0,0 +1,11 @@
+# k = int(input("k ga son kiriting \n >>>>>>>"))
+# n = int(input("n gta son kiriting \n >>>>>>"))
+
+# sonlar = list(range(1,n+1))
+
+# for son in sonlar:
+#     print(f"{son} - sikl : {k}")
+
+
+
+
