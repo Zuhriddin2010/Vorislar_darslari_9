77f747faf197aca7cdaaa340d0f02cdd0bf1e246
Zuhriddin2010 <zuhriddin2010@gmail.com>
2025-11-06 16:08:32 +0500

    hgh

---
+++ masalar_yechish2.py
@@ -0,0 +1,17 @@
+# a = int(input("a ga son kiriting >>>>>>>>>"))
+# b = int(input("b ga son kiriting >>>>>>>>>"))
+
+# g = list(range(a,b+1 ))
+# d = list(range(b,a+1))
+
+
+# for son in g,d:
+#     print(son)
+
+a = int(input("a ga son kiriting >>>>>>>>>"))
+b = int(input("b ga son kiriting >>>>>>>>>>"))
+
+sonlar = list(range(a+1,b))
+
+sonlar.sort(reverse=True)
+print(sonlar)
