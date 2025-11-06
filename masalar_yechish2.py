4a03233982c0fe6c400206664a1a9a42581b5d35
Zuhriddin2010 <zuhriddin2010@gmail.com>
2025-11-06 16:13:56 +0500

    ..

--- masalar_yechish2.py
+++ masalar_yechish2.py
@@ -8,11 +8,12 @@
 # for son in g,d:
 #     print(son)
 
-a = int(input("a ga son kiriting >>>>>>>>>"))
-b = int(input("b ga son kiriting >>>>>>>>>>"))
+# a = int(input("a ga son kiriting >>>>>>>>>"))
+# b = int(input("b ga son kiriting >>>>>>>>>>"))
 
-sonlar = list(range(a+1,b))
+# sonlar = list(range(a+1,b))
 
-sonlar.sort(reverse=True)
-print(sonlar)
-#tttrtrt
+# sonlar.sort(reverse=True)
+# print(sonlar)
+
+print("hello")
