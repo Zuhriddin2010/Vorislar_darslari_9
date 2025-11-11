680bce56d2999fd91eafe6e67d62c36f1011f8b2
zuhriddin <zuhriddinqadamboyev38@gmail.com>
2025-11-11 15:28:49 +0500

    Update 8-dars.py

--- 8-dars.py
+++ 8-dars.py
@@ -4,5 +4,5 @@ print("Let's learn Python programming!")
 a = 5
 b = 10
 c = a + b                   
-print("The sum of", a, "and", b, "is", c)        
+print("The sum of", a, "and", b, "is", c)                                                                   
                             
