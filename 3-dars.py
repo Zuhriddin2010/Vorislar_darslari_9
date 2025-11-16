9ec07a769f2a5bc4e6fe7b6db8b50c58c5e02484
zuhriddin <zuhriddinqadamboyev38@gmail.com>
2025-11-16 11:36:43 +0500

    Update 3-dars.py

--- 3-dars.py
+++ 3-dars.py
@@ -8,7 +8,7 @@
 # x = float(input("x sonni kiriting: x = "))           
 # y = 3*x**6 - 6*x**2 - 7
 # print("y =", y)     
-
+                                   
 a = int(input("a sonni kiriting: a = "))                  
 b = int(input("b sonni kiriting: b = "))
 c = (a+b)/2
