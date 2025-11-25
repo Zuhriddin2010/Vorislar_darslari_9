3207f469213914f5bd5bc34d8050b5189aa8cfa7
zuhriddin <zuhriddinqadamboyev38@gmail.com>
2025-11-25 16:15:54 +0500

    Update united-1.py

--- united-1.py
+++ united-1.py
@@ -1,2 +1,2 @@
 #salom
-#ssssfffgr       
+#ssssfffgr       ![Typing Text](https://readme-typing-svg.demolab.com?lines=Salom!+Men+ismim+Qadamboyev+Zuhriddin+Yoldashboy+ogli.;IT+sohasiga+qiziqaman+maqsadim+web+dasturchi+bo'lish.&font=Fira+Code&size=25&pause=1000&color=0000FF&center=true&vCenter=true&width=1200)
