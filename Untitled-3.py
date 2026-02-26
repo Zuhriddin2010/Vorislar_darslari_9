aa0f755a27bc27569d36fa2379f9717905cbe223
zuhriddin <zuhriddinqadamoyev38@gmail.com>
2026-02-26 17:47:47 +0500

    s

---
+++ Untitled-3.py
@@ -0,0 +1,34 @@
+a = (int(input("1 dan 10 gacha son kiriting men u sonda qanday telefon turganini aytib beraman \n >>>>>>>>>>")))
+
+if a == 1:
+    print(f"siz kiritgan sonda nokiya ")
+
+if a == 0:
+    print(f"siz kiritgan sonda Samsung ")
+
+if a == 2:
+    print(f"siz kiritgan sonda Xiomi")
+
+if a == 3:
+    print(f"siz kiritgan sonda Redmi")
+
+if a == 4:
+    print(f"siz kiritgan sonda Vivo")
+
+if a == 5:
+    print(f"siz kiritgan sonda Texno")
+
+if a == 6:
+    print(f"siz kiritgan sonda Artel")
+
+if a == 7:
+    print(f"siz kiritgan sonda Realme")
+
+if a == 8:
+    print(f"siz kiritgan sonda GL")
+
+if a == 9:
+    print(f"siz kiritgan sonda infinix")
+
+if a == 10:
+    print(f"siz kiritgan sonda Galaxy")
